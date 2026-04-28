abda8cdec0356a181b8d1fc068294d8d5a219f2a
bleze564 <153006729+bleze564@users.noreply.github.com>
2026-04-28 21:02:29 +0300

    21312
    qw

--- src/components/SideParts/header/Header.jsx
+++ src/components/SideParts/header/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Logo } from '../../../assets/logo.svg';
+import { ReactComponent as Logo } from '../../../assets/logo.svg'   ;
 
 const Header = () => {
   const Box = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-
   `;
+  const AnchorStyle = styled.div`
+    display: flex;
+    gap: 42px;
+  `;
+   const LogoDiv =styled
   return (
     <Box>
       <Logo width={82} height={56} />
-      <p>Who we are</p>
-      <p>Contacts</p>
-      <p>Menu</p>
+      <AnchorStyle>
+        <p>Who we are</p>
+        <p>Contacts</p>
+        <p>Menu</p>
+      </AnchorStyle>
     </Box>
   );
 };
